25b231754f4e87b9089bc46bbeb38118227e283b
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-27 12:11:06 +0200

    step 5

--- index.js
+++ index.js
@@ -34,6 +34,32 @@ app.get("/search", (req, res) => {
     }
 });
 
+const movies = [
+    { title: "Jaws", year: 1975, rating: 8 },
+    { title: "Avatar", year: 2009, rating: 7.8 },
+    { title: "Brazil", year: 1985, rating: 8 },
+    { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
+
+app.get("/movies/create", (req, res) => {
+    res.send(`create`);
+});
+
+app.get("/movies/read", (req, res) => {
+    res.send({
+        status: 200,
+        data: movies,
+    });
+});
+
+app.get("/movies/update", (req, res) => {
+    res.send(`update`);
+});
+
+app.get("/movies/delete", (req, res) => {
+    res.send(`delete`);
+});
+
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 });
