149466ccc2323348d950efc98203317912bbe08c
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-27 11:53:24 +0200

    step 4

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ app.get("/time", (req, res) => {
     res.send({ status: 200, message: time });
 });
 
+app.get("/hello/:id", (req, res) => {
+    res.send({ status: 200, message: `Hello, ${req.params.id}` });
+});
+
+app.get("/search", (req, res) => {
+    if (req.query.s == undefined || req.query.s == "") {
+        res.status(500);
+        res.send({
+            status: 500,
+            error: true,
+            message: "you have to provide a search",
+        });
+    } else {
+        res.status(200);
+        res.send({ status: 200, message: `ok`, data: req.query.s });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 });
